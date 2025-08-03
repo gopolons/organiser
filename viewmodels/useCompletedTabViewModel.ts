8c81bddee447acf3ac2b2a29482941e92c7a1dac
Georgii Polonskii <gopolons@gmail.com>
2025-08-03 15:42:34 +0100

    added a completed tab view model

---
+++ viewmodels/useCompletedTabViewModel.ts
@@ -0,0 +1,45 @@
+import { TaskData } from "@/model/task";
+import { TaskPersistence } from "@/services/persistence";
+import { useState } from "react";
+
+export default function useCompletedTabViewModel(persistence: TaskPersistence) {
+  // Variables for keeping track of loading state
+  const [loading, setLoading] = useState(false);
+
+  // Function for fetching data from persistence & handling errors appropriately
+  const fetchCompletedTasksFromPersistence = async () => {
+    setLoading(true);
+    let data: TaskData[] = [];
+    try {
+      data = await persistence.getCompletedTasks();
+    } catch (error) {
+      return {
+        success: false,
+        error: error instanceof Error ? error.message : String(error),
+      };
+    } finally {
+      setLoading(false);
+    }
+
+    return { success: true, data };
+  };
+
+  // Function for toggling task status in persistence & handling errors appropriately
+  const toggleTaskStatus = async (taskId: string) => {
+    setLoading(true);
+    try {
+      await persistence.toggleTaskStatus(taskId);
+    } catch (error) {
+      return {
+        success: false,
+        error: error instanceof Error ? error.message : String(error),
+      };
+    } finally {
+      setLoading(false);
+    }
+
+    return { success: true };
+  };
+
+  return { fetchCompletedTasksFromPersistence, toggleTaskStatus, loading };
+}
